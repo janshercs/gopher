71b7b2783a574b5358f50e249ca9d7acb57c3976
janshercs <jansen.cs@gmail.com>
2021-10-30 12:37:28 +0800

    Completed roman.

--- roman/roman.go
+++ roman/roman.go
@@ -2,12 +2,52 @@ package main
 
 import "strings"
 
-type RomanNumeral struct {
-	Arabic int
-	Roman  string
+func ConvertToRoman(arabic uint16) string {
+	var result strings.Builder
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Arabic {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Arabic
+		}
+	}
+	return result.String()
 }
 
-var allRomanNumerals = []RomanNumeral{
+func ConvertToArabic(roman string) (total uint16) {
+	for _, symbols := range windowedRoman(roman).Symbols() {
+		total += allRomanNumerals.ValueOf(symbols...)
+	}
+	return total
+}
+
+type RomanNumeral struct {
+	Arabic uint16
+	Symbol string
+}
+
+type RomanNumerals []RomanNumeral
+
+func (r RomanNumerals) ValueOf(symbols ...byte) uint16 {
+	symbol := string(symbols)
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return s.Arabic
+		}
+	}
+	return 0
+}
+
+func (r RomanNumerals) Exists(symbols ...byte) bool {
+	symbol := string(symbols)
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return true
+		}
+	}
+	return false
+}
+
+var allRomanNumerals = RomanNumerals{
 	{1000, "M"},
 	{900, "CM"},
 	{500, "D"},
@@ -23,13 +63,24 @@ var allRomanNumerals = []RomanNumeral{
 	{1, "I"},
 }
 
-func ConvertToRoman(arabic int) string {
-	var result strings.Builder
-	for _, numeral := range allRomanNumerals {
-		for arabic >= numeral.Arabic {
-			result.WriteString(numeral.Roman)
-			arabic -= numeral.Arabic
+type windowedRoman string
+
+func (w windowedRoman) Symbols() [][]byte {
+	var symbols [][]byte
+	for i := 0; i < len(w); i++ {
+		symbol := w[i]
+		notAtEnd := i+1 < len(w)
+
+		if notAtEnd && isSubtractive(symbol) && allRomanNumerals.Exists(symbol, w[i+1]) {
+			symbols = append(symbols, []byte{symbol, w[i+1]})
+			i++
+		} else {
+			symbols = append(symbols, []byte{symbol})
 		}
 	}
-	return result.String()
+	return symbols
+}
+
+func isSubtractive(symbol uint8) bool {
+	return symbol == 'I' || symbol == 'X' || symbol == 'C'
 }
--- roman/roman_test.go
+++ roman/roman_test.go
@@ -3,43 +3,44 @@ package main
 import (
 	"fmt"
 	"testing"
+	"testing/quick"
 )
 
-func TestSomething(t *testing.T) {
-	cases := []struct {
-		Arabic int
-		Roman  string
-	}{
-		{1, "I"},
-		{2, "II"},
-		{3, "III"},
-		{4, "IV"},
-		{5, "V"},
-		{6, "VI"},
-		{9, "IX"},
-		{10, "X"},
-		{10, "X"},
-		{14, "XIV"},
-		{18, "XVIII"},
-		{20, "XX"},
-		{39, "XXXIX"},
-		{40, "XL"},
-		{47, "XLVII"},
-		{49, "XLIX"},
-		{50, "L"},
-		{90, "XC"},
-		{100, "C"},
-		{400, "CD"},
-		{500, "D"},
-		{900, "CM"},
-		{1000, "M"},
-		{1984, "MCMLXXXIV"},
-		{3999, "MMMCMXCIX"},
-		{2014, "MMXIV"},
-		{1006, "MVI"},
-		{798, "DCCXCVIII"},
-	}
+var cases = []struct {
+	Arabic uint16
+	Roman  string
+}{
+	{1, "I"},
+	{2, "II"},
+	{3, "III"},
+	{4, "IV"},
+	{5, "V"},
+	{6, "VI"},
+	{9, "IX"},
+	{10, "X"},
+	{10, "X"},
+	{14, "XIV"},
+	{18, "XVIII"},
+	{20, "XX"},
+	{39, "XXXIX"},
+	{40, "XL"},
+	{47, "XLVII"},
+	{49, "XLIX"},
+	{50, "L"},
+	{90, "XC"},
+	{100, "C"},
+	{400, "CD"},
+	{500, "D"},
+	{900, "CM"},
+	{1000, "M"},
+	{1984, "MCMLXXXIV"},
+	{3999, "MMMCMXCIX"},
+	{2014, "MMXIV"},
+	{1006, "MVI"},
+	{798, "DCCXCVIII"},
+}
 
+func TestArabicToRoman(t *testing.T) {
 	for _, test := range cases {
 		t.Run(fmt.Sprintf("%d gets converted to %q", test.Arabic, test.Roman), func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
@@ -48,5 +49,32 @@ func TestSomething(t *testing.T) {
 				t.Errorf("got %q but wanted %q", got, test.Roman)
 			}
 		})
+	}
+}
+
+func TestRomanToArabic(t *testing.T) {
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("%d gets converted to %q", test.Arabic, test.Roman), func(t *testing.T) {
+			got := ConvertToArabic(test.Roman)
+
+			if got != test.Arabic {
+				t.Errorf("got %d but wanted %d", got, test.Arabic)
+			}
+		})
+	}
+}
+
+func TestPropertiesOfConversion(t *testing.T) {
+	assertion := func(arabic uint16) bool {
+		if arabic > 3999 {
+			return true
+		}
+		roman := ConvertToRoman(arabic)
+		fromRoman := ConvertToArabic(roman)
+		return fromRoman == arabic
+	}
+
+	if err := quick.Check(assertion, nil); err != nil {
+		t.Error("failed checks", err)
 	}
 }
